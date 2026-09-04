a1c4aabf6aa11f67fbdf8ae298412066e6743bdb
teniks <45620410+teniks@users.noreply.github.com>
2026-09-04 20:23:44 +0300

    feat:add model and configuration for SQLite

---
+++ Core/Models/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CatchLightning.Core.Models
+{
+    internal class Goal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public int? IdCategory { get; set; }
+        public Category? Category { get; set; }
+
+        public List<Achievement> Achivments { get; } = new();
+    }
+}
