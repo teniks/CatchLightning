f9ba4517da0d866ff97f6625e93f055dca229797
teniks <45620410+teniks@users.noreply.github.com>
2026-09-22 17:11:21 +0300

    feat: add EntityExistValidator and NotExist error

---
+++ Core/Services/Validators/EntityExistValidator.cs
@@ -0,0 +1,29 @@
+﻿using CatchLightning.Core.Abstractions;
+using System.Threading.Tasks;
+
+namespace CatchLightning.Core.Services.Validators
+{
+    public class EntityExistValidator<TEntity, TEntityErrors>
+        : IValidator<TEntity, TEntityErrors>
+        where TEntity : IEntity
+    {
+        private IGetterById<TEntity> _getterById;
+        private TEntityErrors _errorDuplicate;
+
+
+        public EntityExistValidator(IGetterById<TEntity> getterById, TEntityErrors errorDuplicate)
+        {
+            _getterById = getterById;
+            _errorDuplicate = errorDuplicate;
+        }
+
+        public async Task<TEntityErrors?> Validate(TEntity entity)
+        {
+            var result = await _getterById.GetByIdAsync(entity.Id);
+
+            if (result is null)
+                return _errorDuplicate;
+            return default(TEntityErrors?);
+        }
+    }
+}
--- Core/Services/Validators/ValidationErrors.cs
+++ Core/Services/Validators/ValidationErrors.cs
@@ -5,18 +5,21 @@
         NameRequired,
         NameDuplicated,
         GoalNotFound,
-        CategoryNotFound
+        CategoryNotFound,
+        NotExist
     }
 
     public enum CategoryError
     {
         NameRequired,
         NameDuplicated,
+        NotExist
     }
 
     public enum GoalError
     {
         NameRequired,
         NameDuplicated,
+        NotExist
     }
 }
